0e9693e40b395a2e74056d7e7e9ca267669facd0
Asaf <Asaf>
2015-12-30 11:06:36 +0200

    fixed

--- SRC_WIN/solution/LogWriteable.java
+++ SRC_WIN/solution/LogWriteable.java
@@ -11,37 +11,27 @@ public class LogWriteable implements WritableComparable<LogWriteable> {
 	
 	private Text hostname;
 	private Text username;
-	private Text date;
-	private Text hour;
 	
 	public LogWriteable() {
 		this.hostname = new Text();
 		this.username = new Text();
-		this.date = new Text();
-		this.hour = new Text();
 	}
 	
-	public LogWriteable(String hostname, String date, String hour, String username) {
+	public LogWriteable(String hostname, String username) {
 		this.hostname = new Text(hostname);
 		this.username = new Text(username);
-		this.date = new Text(date);
-		this.hour = new Text(hour);
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
 		this.hostname.write(out);
 		this.username.write(out);
-		this.date.write(out);
-		this.hour.write(out);
 	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
 		this.hostname.readFields(in);
 		this.username.readFields(in);
-		this.date.readFields(in);
-		this.hour.readFields(in);
 	}
 	
 
@@ -52,13 +42,13 @@ public class LogWriteable implements WritableComparable<LogWriteable> {
 	
 	@Override
 	public String toString() {
-	    return this.username.toString()+ " " + this.hostname.toString() + " " + this.date.toString();
+	    return this.username.toString()+ " " + this.hostname.toString();
 	}
 	
 	@Override
 	public boolean equals(Object o) {
 		if(o instanceof LogWriteable) {
-			return this.hostname.equals(((LogWriteable) o).hostname) && this.username.equals(((LogWriteable) o).username) && this.date.equals(((LogWriteable) o).date);
+			return this.hostname.equals(((LogWriteable) o).hostname) && this.username.equals(((LogWriteable) o).username);
 
 		}
 		return false;
