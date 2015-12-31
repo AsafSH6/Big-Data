051eb6de16bf241b5fc79168679ce121b3e5272f
Asaf <Asaf>
2015-12-31 07:53:15 +0200

    fixing task3

--- SRC_WIN/solution/Map.java
+++ SRC_WIN/solution/Map.java
@@ -11,8 +11,15 @@ public class Map extends MapReduceBase implements Mapper<LongWritable, Text, Log
 
 		String[] lines = value.toString().split(" ");
 		String[] relevantLines = new String[2];
-		relevantLines[0] = lines[4];
-		relevantLines[1] = lines[31];
+		for(int i=0, int j=0; i < lines.length; i++) {
+			if(!lines[i].equals("")) {
+				if(j == 1)
+					relevantLines[0] = lines[i];
+				else if(j == 5)
+					relevantLines[1] = lines[i];
+				++j;
+			}
+		}
 				output.collect(new LogWriteable(relevantLines[0].split("/")[2], relevantLines[1]), new IntWritable(1));
 	}
 }
